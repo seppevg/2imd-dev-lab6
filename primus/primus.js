5b4d43a3d8a3b40d0644a1e8a575cee98ee2c4cb
Seppe Vangeel <seppe.vg@live.be>
2021-04-08 19:38:12 +0200

    fix: remove uneccesary data log in console

--- primus/primus.js
+++ primus/primus.js
@@ -3,10 +3,9 @@ const Primus = require('primus');
 const go = (server) => {
     const primus = new Primus(server, {/* options */ });
     primus.on('connection', (spark) => {
-        console.log('Received spark');
-
+        //console.log('Received spark');
         spark.on('data', (data) => {
-            console.log(data);
+            //console.log(data);
             primus.write(data);
         });
     });
