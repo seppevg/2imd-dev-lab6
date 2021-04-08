809989538415f3405b80f3cd5b6632bb1fb156eb
Seppe Vangeel <seppe.vg@live.be>
2021-04-08 15:55:29 +0200

    fix: delete user-route, add folder for data, use data folder

--- routes/index.js
+++ routes/index.js
@@ -1,68 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
-const players = [
-  {
-    "id": "van-gerwen",
-    "name": "Michael van Gerwen",
-    "nickname": "De Groene Sloopkogel",
-    "score": 11
-  },
-  {
-    "id": "vd-bergh",
-    "name": "Dimitri van den Bergh",
-    "nickname": "Dancing Dimi",
-    "score": 21
-  },
-  {
-    "id": "clayton",
-    "name": "Jonny Clayton",
-    "nickname": "The Ferret",
-    "score": 20
-  },
-  {
-    "id": "aspinall",
-    "name": "Nathan Aspinall",
-    "nickname": "The Asp",
-    "score": 18
-  },
-  {
-    "id": "cross",
-    "name": "Rob Cross",
-    "nickname": "Voltage",
-    "score": 15
-  },
-  {
-    "id": "anderson",
-    "name": "Gary Anderson",
-    "nickname": "The Flying Scotsman",
-    "score": 19
-  },
-  {
-    "id": "wright",
-    "name": "Peter Wright",
-    "nickname": "Snakebite",
-    "score": 16
-  },
-  {
-    "id": "de-sousa",
-    "name": "José de Sousa",
-    "nickname": "The Special One",
-    "score": 14
-  },
-  {
-    "id": "wade",
-    "name": "James Wade",
-    "nickname": "The Machine",
-    "score": 11
-  },
-  {
-    "id": "durrant",
-    "name": "Glen Durrant",
-    "nickname": "Duzza",
-    "score": 8
-  }
-];
+const players = require("../data/players").players;
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
